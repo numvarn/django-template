c1a88114851dcfd251d7c8b7d590f5caba729637
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2026-07-13 10:30:08 +0700

    setup static files

--- django_template/settings.py
+++ django_template/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'django_template.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'statics',]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
