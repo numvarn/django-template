81bdd2fc09e63a3c6a26a5e281e2fd0f1323899d
Phisan Sookkhee <phisan.shukkhi@gmail.com>
2026-07-13 11:43:55 +0700

    feat: implement dashboard and page templates with associated views and routes

--- django_template/settings.py
+++ django_template/settings.py
@@ -125,7 +125,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'statics',]
+STATICFILES_DIRS = [
+    BASE_DIR / 'statics',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
--- web/views.py
+++ web/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, 'index.html')
 
 def about(request):
-    return HttpResponse('about')
-
+    return render(request, 'about.html')
+    
 def contact(request):
-    return HttpResponse('contact')
+    return render(request, 'contact.html')
